c04eaeb8dd129af2cee67c3721a34ef6b2d7da85
pdxgrantc <pdxgrantc@gmail.com>
2026-04-02 17:35:33 -0700

    basic ez budget page

--- src/routes/about-me_/server-management.tsx
+++ src/routes/about-me_/server-management.tsx
@@ -10,40 +10,37 @@ function RouteComponent() {
     <PageWrapper>
       <div className="">
         <div className="prose">
-          <h1>Server Management Experience</h1>
+          <h1>Home Server Management</h1>
           <p>
             My home server environment is managed using Proxmox as the primary
-            hypervisor, which serves as the central platform for virtualization,
-            resource allocation, and system monitoring. Proxmox allows multiple
-            services to be isolated into dedicated virtual machines, improving
-            reliability, security, and maintainability while making it easier to
-            manage updates and troubleshoot individual components without
-            affecting the entire system.
+            hypervisor, which serves as the central platform for me to explore
+            virtualization, resource allocation, and system monitoring. Proxmox
+            allows me to host multiple services each isolated into dedicated
+            virtual machines or containers.
           </p>
           <p>
             The server hosts several purpose-built virtual machines. A Jellyfin
             VM is used for self-hosted media streaming, providing local control
-            over media libraries and client access across devices. Remote access
-            to the home network is handled through an OpenVPN virtual machine,
-            enabling secure connections for management and file access while
-            away from the local network. Storage services are provided by a
-            TrueNAS virtual machine, which manages a ZFS RAID-Z1 storage pool
-            shared over SMB. This configuration provides redundancy against
-            single-disk failure, supports snapshots, and allows multiple systems
-            on the network to access centralized storage reliably.
+            over media libraries and client access across devices on my local
+            network. Remote access to the home network is handled through an
+            OpenVPN virtual machine, enabling me to establish secure connections
+            to my home network. I have a NAS (network attached storage) server
+            providing a space for media, personal storage, and backups
+            accessible to of my computers. This is hosted on dedicated VM
+            running TrueNAS, which manages a ZFS RAID-Z1 storage pool shared
+            over SMB. This configuration provides redundancy against single-disk
+            failure, supports snapshots, and allows other VMs and computers to
+            securely to access centralized storage reliably.
           </p>
           <p>
             In addition to these services, the server includes a dedicated
-            virtual machine for running Docker containers. This VM is used to
-            deploy and manage containerized applications, allowing rapid
-            experimentation and clean separation from core infrastructure
-            services. One of the primary containers hosted in this environment
-            is Immich, which is used for self-hosted photo management and
-            backup. By containerizing services like Immich, the system benefits
-            from simplified updates, portability, and efficient resource usage.
-            Overall, this home server setup emphasizes flexibility, service
-            isolation, data integrity, and long-term maintainability while
-            supporting a wide range of personal infrastructure needs.
+            virtual machine for running Docker containers. Currently I have an
+            Immich instance running allowing me to automatically store and
+            catalog my photos. By containerizing services
+            like Immich, the system benefits from simplified updates, easy deployment,
+            portability, and efficient resource usage. I have attempted to setup my home server
+            to emphasize flexibility, service isolation, data integrity while allowing me to keep
+            long-term maintainability.
           </p>
         </div>
       </div>
--- src/routes/projects_/ez-budget.tsx
+++ src/routes/projects_/ez-budget.tsx
@@ -1,3 +1,4 @@
+import { PageWrapper } from '@/components/PageWrapper'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/projects_/ez-budget')({
@@ -5,5 +6,37 @@ export const Route = createFileRoute('/projects_/ez-budget')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/EzBudget"!</div>
+  return (
+    <PageWrapper>
+      <div className="prose">
+        <h1>Ez-Budget Web Application</h1>
+        <h2>Overview</h2>
+        <p>
+          Ez-Budget is a web application designed to help users manage their
+          finances better by providing a simple an intuitive interface for
+          tracking expenses and income. I wanted to create this application to
+          help me categorize my spending over time to build better financial
+          habits.
+        </p>
+        <h2>Architecture</h2>
+        <p>
+          The application is built using React as a single page application to
+          serve the frontend from a single endpoint. The client side then makes
+          calls to both Google Oauth for user authentication and custom API
+          endpoints to manage user data. Both components are served from Google
+          cloud using Cloud Run. My database of choice for this project is the
+          Datastore document database, allowing me to use a simple schema that
+          separates user data into multiple document trees. Keeping user data
+          separate for better security.
+        </p>
+        <h2>Tech Stack</h2>
+        <p>
+          The application is built using a React frontend powered by a Flask API
+          to handle user data. The application is hosted via Google cloud using
+          Cloud Run. My database of choice for this project is the Datastore
+          document database.
+        </p>
+      </div>
+    </PageWrapper>
+  )
 }
